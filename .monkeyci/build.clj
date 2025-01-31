ca97a15c59245bdad7486a3b5416d858058b6bac
Wout Neirynck <wout@monkey-projects.be>
2025-01-31 12:07:03 +0100

    Added publish config

--- .monkeyci/build.clj
+++ .monkeyci/build.clj
@@ -4,11 +4,15 @@
              [clj :as clj]
              [github :as gh]]))
 
+(defn- library-config [dir]
+  {:test-job-id (str dir "-test")
+   :publish-job-id (str dir "-publish")
+   :artifact-id (str dir "-test-junit")})
+
 (defn build-lib [dir]
   (fn [ctx]
-    (->> ((clj/deps-library
-           {:test-job-id (str dir "-test")
-            :publish-job-id (str dir "-publish")}) ctx)
+    (->> ((clj/deps-library (library-config dir)) ctx)
+         ;; Set work dir on each of the jobs
          (map #(m/work-dir % dir)))))
 
 [(build-lib "core")
